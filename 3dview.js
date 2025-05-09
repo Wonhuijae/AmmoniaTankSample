b005e902818886473cc101c068fff69a95840599
Wonhuijae <95402939+Wonhuijae@users.noreply.github.com>
2025-05-09 17:07:21 +0900

    회전 방향 제한, 조명 설정

--- 3dview.js
+++ 3dview.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three'
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { DragControls } from 'three/examples/jsm/controls/DragControls.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
@@ -36,15 +39,26 @@ camera.position.set(0, 0, -5)
 scene.add(camera)
 
 // 카메라 컨트롤
-const controls = new TrackballControls(camera, renderer.domElement)
-controls.rotateSpeed = 0.25;
-controls.enableRotate = true;
-controls.panSpeed = 0.05;     // 팬 속도
-controls.zoomSpeed = 0.5
-controls.staticMoving = false;
-controls.dynamicDampingFactor = 0.1
-controls.minDistance = 0.25;
-controls.maxDistance = 1.5;
+// const trackballControls = new TrackballControls(camera, renderer.domElement)
+// trackballControls.rotateSpeed = 0.25;
+// trackballControls.enableRotate = true;
+// trackballControls.panSpeed = 0.05;     // 팬 속도
+// trackballControls.zoomSpeed = 0.5
+// trackballControls.staticMoving = false;
+// trackballControls.dynamicDampingFactor = 0.1
+// trackballControls.minDistance = 0.25;
+// trackballControls.maxDistance = 1.5;
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
+orbitControls.minDistance = 0.2;
+orbitControls.maxDistance = 2;
+orbitControls.enableRotate = false;
+orbitControls.zoomSpeed = 0.25;
+orbitControls.enablePan = false;
+
+let transformControls;
+let dragControls;
 
 // 음영처리
 const spotLight = new THREE.SpotLight(0xffffff, 1.5, 2, -90 * Math.PI / 180, 0.3, 1);
@@ -62,26 +76,18 @@ loader.load(
         // model = object;
         model = object.scene;
         model.scale.set(0.1, 0.1, 0.1)
-        model.rotation.y = -90 * Math.PI / 180
-
-        // 바운딩 박스
-        const box = new THREE.Box3().setFromObject(model);
-        const center = new THREE.Vector3();
-        let centerPos = box.getCenter(center); // 중심점을 구한다.
-        // center.x += 0.5
 
         model.castShadow = true
         model.receiveShadow = true;
 
-        spotLight.position.copy(centerPos)
-        spotLight.position.y += 1
+        // transformControls = new TransformControls(camera, renderer.domElement)
+        // scene.add(transformControls)
 
-        camera.position.copy(model.position)
-        camera.position.z -= 0.6
-        scene.add(model);
+        dragControls = new DragControls(model, camera, renderer.domElement);
 
-        camera.lookAt(centerPos)
-        controls.target.copy(centerPos);
+        scene.add(model)
+        reset();
+        // trackballControls.target.copy(centerPos);
     }
 )
 
@@ -110,13 +116,55 @@ window.addEventListener('resize', () => {
 function animate() {
     requestAnimationFrame(animate);
 
-    controls.update();
+    // trackballControls.update();
     renderer.render(scene, camera);
 }
 animate();
 
 function reset() {
-    controls.reset()
+    // trackballControls.reset()
+    orbitControls.reset();
+
+    model.position.set(0, 0, 0)
+    model.rotation.set(0.45, 1, 0)
     camera.position.copy(model.position)
-    camera.position.z -= 0.6
+    camera.position.y += 0.1;
+    camera.rotation.set(0, 0, 0)
+    camera.position.z += 0.65
+
+    spotLight.position.copy(camera.position)
+
+    // 바운딩 박스
+    const box = new THREE.Box3().setFromObject(model);
+    const center = new THREE.Vector3();
+    let centerPos = box.getCenter(center); // 중심점을 구한다.
+
+    camera.lookAt(centerPos)
 }
+
+let isDragging = false;
+let lastMousePos = new THREE.Vector2();
+
+renderer.domElement.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    lastMousePos.set(event.clientX, event.clientY);
+});
+
+renderer.domElement.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        const deltaX = event.clientX - lastMousePos.x;
+        //const deltaY = event.clientY - lastMousePos.y;
+
+        const rotationSpeed = 0.005;
+        model.rotation.y += deltaX * rotationSpeed;
+        //model.rotation.x += deltaY * rotationSpeed;
+
+        lastMousePos.set(event.clientX, event.clientY);
+    }
+});
+
+renderer.domElement.addEventListener('mouseup', () => {
+    isDragging = false;
+
+    console.log(model.rotation)
+});
